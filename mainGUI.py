0c8504d6e4be1b622d5fbc435c0a34beee480295
JNichols4 <jordan.nichols04@gmail.com>
2019-01-18 14:31:54 -0600

    Initial creation of the GUI file.

---
+++ mainGUI.py
@@ -0,0 +1,7 @@
+# Starting this file in the hopes that I will be able to create a graphical user interface that incorporates the
+# ability to change the files imported.
+#
+# TODO: Create a GUI that allows for file selection, single (or multiple) files for parsing.
+# TODO: Create a GUI option that allows for directory scanning, "new" file parsing, and mass data generation.
+#
+# Making sure to push to git so that this file is available online.
